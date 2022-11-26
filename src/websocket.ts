3240193288e8fa94e715763cbb8984188e70b9fb
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2022-11-25 22:47:05 -0300

    Alterações

--- src/websocket.ts
+++ src/websocket.ts
@@ -3,19 +3,13 @@ import { io } from "./http";
 
 
 io.on("connection", (socket) => {
-    console.log(`User Connection ${socket.id}`)
     console.log("Connection successfully established!");
-
-
-
-    // Recebendo e listando users online
     socket.on("userOnline", (data) => {
 
       console.log(`acabou de entrar`)
 
    })
-
-  
+ 
     socket.on("select_room", (data, callback)=> {
     });
 
